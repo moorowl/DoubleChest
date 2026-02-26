0344b11f8a74372ce921fe3b81ffb262d1c22f71
moorowl <moorowl@outlook.com>
2026-02-26 17:57:37 +0000

    Bump version to 1.0.3 + Fix chest missing AutomatedStorageAuthoring

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace DoubleChest {
     public class Main : IMod {
-	    public const string Version = "1.0.2";
+	    public const string Version = "1.0.3";
 	    public const string InternalName = "DoubleChest";
 	    public const string DisplayName = "Paintable Double Chest";
         
