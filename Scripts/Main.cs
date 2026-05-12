677001c5624f15437fde6f9a65b2f22854e9fa65
moorowl <moorowl@outlook.com>
2026-05-13 00:23:28 +0100

    Bump version to 1.1.1

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace DoubleChest {
     public class Main : IMod {
-	    public const string Version = "1.1";
+	    public const string Version = "1.1.1";
 	    public const string InternalName = "DoubleChest";
 	    public const string DisplayName = "Paintable Double Chest";
         
